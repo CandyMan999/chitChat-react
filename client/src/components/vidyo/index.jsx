d584cb897696e15b69b37c2f8581bf170aabaed6
John Lewis <jalewis1221@gmail.com>
2018-11-05 13:18:48 -0600

    added video chat

--- client/src/components/vidyo/index.jsx
+++ client/src/components/vidyo/index.jsx
@@ -1,10 +1,46 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 class Vidyo extends Component {
-  state = {};
+  state = {
+    roomId: null
+  };
+
+  makeRoomid = () => {
+    let text = "";
+    const possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < 7; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    this.setState({ roomId: text });
+    console.log("chat roomId: ", this.state.roomId);
+  };
+
   render() {
     return (
-      <div id="freshtilledsoil_embed_widget" className="video-chat-widget" />
+      <Fragment>
+        {" "}
+        {this.state.roomId ? (
+          <div>
+            {" "}
+            <button style={{ position: "absolute" }}>
+              test {this.state.roomId}
+            </button>
+            <iframe
+              title="chatshit"
+              src={`https://tokbox.com/embed/embed/ot-embed.js?embedId=6ee527d4-9d0f-43eb-b042-b6cae60f360f&room=default&iframe=true`}
+              width="800px"
+              height="640px"
+              allow="microphone; camera"
+            />
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.makeRoomid}>Join this chat</button>
+          </div>
+        )}
+      </Fragment>
     );
   }
 }
