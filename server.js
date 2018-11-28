9003fdcf1b763527f9c7c16718925bdef8243aa8
John Lewis <jalewis1221@gmail.com>
2018-11-27 18:46:35 -0600

    Add dotenv for env vars

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const fs = require("fs");
 const formidable = require("express-formidable");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
